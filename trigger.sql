f3a92aa70f4fc3109b8641e085c2e3542520ae27
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-05-17 23:20:29 -0300

    Correções realizadas

--- trigger.sql
+++ trigger.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER prevent_employees_day BEFORE INSERT ON employees
     BEGIN
 	DECLARE current_day VARCHAR(3);
     SET current_day = date_format(CURRENT_DATE, '%a'); -- pega o dia atual
-    IF current_day IN ('Sat','Sun','Mon') THEN -- aplicar correções de dia??
+    IF current_day IN ('Sat','Sun','Mon') THEN
 
     SIGNAL SQLSTATE '45000'
     SET MESSAGE_TEXT = 'Você não pode realizar alterações na tabela employees durante este dia!';
