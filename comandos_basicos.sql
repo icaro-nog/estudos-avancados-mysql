929b42a3b911dbec8e0fef57f0b3353097b3f7be
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-05-20 13:49:30 -0300

    comandos básicos sql

---
+++ comandos_basicos.sql
@@ -0,0 +1,9 @@
+-- versão do banco de dados e data atual
+select version(), current_date();
+
+-- utilizando mysql como calculadora
+select SIN(PI()/4), (4+1)*5;
+
+select now();
+
+select USER(), current_date();
